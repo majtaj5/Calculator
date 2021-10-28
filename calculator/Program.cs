4dd709c99e4347e36f29b30ee0638d5be2ff0d63
Piotr Krol <krolpiotr225@gmail.com>
2021-10-28 21:05:29 +0200

    delete

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,18 +8,8 @@ namespace calculator
         
         static void Main(string[] args)
         {
-            //Calculator choice = new Calculator();
-            //string a = choice.Choice();
-            int number1, number2, sum;
-            Console.WriteLine("Podaj dwie liczby calkowite: ");
-            number1 = int.Parse(Console.ReadLine());
-            number2 = int.Parse(Console.ReadLine());
-            sum = Calculator.Sum(number1, number2);
-            Console.WriteLine($"Suma liczb {number1} oraz {number2} wynosi: {sum}");
+            
 
-            Console.WriteLine("Wybierz gre 1,2,3,4");
-
- 
 
             Console.ReadKey();
         }
