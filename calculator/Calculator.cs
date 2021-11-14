87209df232eda514189349f158a8c569c1ac2061
PyLukasz <as.lukasz@gmail.com>
2021-11-14 18:43:59 +0100

    Dodanie metody na odejmowanie dwoch liczb calkowitych.

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -4,6 +4,9 @@ namespace calculator
 {
     public class Calculator
     {
-
+        public static int Subtraction(int number1, int number2)
+        {
+            return number1 - number2;
+        }
     }
 }
