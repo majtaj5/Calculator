74331935c12253d60ac7a05172ab624b45241c21
PyLukasz <as.lukasz@gmail.com>
2021-11-14 19:25:17 +0100

    Poprawa nazwy metody v2.

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -4,7 +4,7 @@ namespace calculator
 {
     public class Calculator
     {
-        public int Add(int number1, int number2)
+        public int Addition(int number1, int number2)
         {            
             return number1 + number2;
         }
