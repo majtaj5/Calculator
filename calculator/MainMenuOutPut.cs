ed54cbb699b21990859c6c548c52b931f40dc4e4
Piotr Krol <krolpiotr225@gmail.com>
2021-11-15 19:49:30 +0100

    I created a printout of the main menu text.

---
+++ calculator/MainMenuOutPut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class MainMenuOutPut
+    {
+        public static void ShowMenuOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("What would you like to do?");
+            string[] mainoptions = { "0 - simple calculator", "1 - advanced calculator", "2 - working with the text files" };
+
+            foreach (string userInput in mainoptions)
+            {
+                Console.WriteLine(userInput);
+            }
+            Console.ResetColor();
+        }
+    }
+}
+
+
+       
