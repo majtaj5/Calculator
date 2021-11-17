1be61fb5c104cc30db963e28523cb7061d61f7f1
PyLukasz <as.lukasz@gmail.com>
2021-11-17 19:52:49 +0100

    Dodanie metody sprawdzającą czy podana liczba jest liczbą pierwszą.

--- calculator/SimpleAlgoritms.cs
+++ calculator/SimpleAlgoritms.cs
@@ -4,6 +4,17 @@ namespace calculator
 {
     public class SimpleAlgoritms
     {
-
+        public static bool PrimeNumber(int number)
+        {
+            double range = number / 2;
+            if (number <= 1) return false;
+            if (number == 2 || number == 3) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i <= range; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
     }
 }
