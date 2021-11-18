b0db761972555baddb30a4062e9ad955566fa9a9
Piotr Krol <krolpiotr225@gmail.com>
2021-11-18 20:48:28 +0100

    Create a method to change the color of the text

---
+++ calculator/ChangeTheColorOfTheText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class ChangeTheColorOfTheText
+    {
+        public static void PrintColorMessage(ConsoleColor color, string message)
+        { 
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+            //Console.ResetColor does not work .I don't know why .I created a method that resets color. Error CS0201  Only assignment, call, increment, decrement, await, and new object expressions can be used as a statement calculator  
+            resetConsole();
+        }
+
+        private static void resetConsole()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        }
+            
+}
