680af9ea6825e838fa462f783f245d8ffacac25c
PyLukasz <as.lukasz@gmail.com>
2021-11-19 21:45:02 +0100

    Usunietgo z metody PrimeNumber "static", usunieto warunek if, petla for zaczyna sie od 2 a nie od 3.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -7,6 +7,9 @@ namespace calculator
     {
         static void Main(string[] args)
         {
+            var test = new SimpleAlgoritms();
+            bool a = test.PrimeNumber(7);
+            Console.WriteLine(a);
             Console.ReadLine();
         }
     }
--- calculator/SimpleAlgoritms.cs
+++ calculator/SimpleAlgoritms.cs
@@ -4,13 +4,12 @@ namespace calculator
 {
     public class SimpleAlgoritms
     {
-        public static bool PrimeNumber(int number)
+        public bool PrimeNumber(int number)
         {
             double range = number / 2;
             if (number <= 1) return false;
-            if (number == 2 || number == 3) return true;
             if (number % 2 == 0) return false;
-            for (int i = 3; i <= range; i++)
+            for (int i = 2; i <= range; i++)
             {
                 if (number % i == 0) return false;
             }
