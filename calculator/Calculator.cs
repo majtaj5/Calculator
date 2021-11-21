4989d71a6ea3402b765a43e19318736ca8394f6f
Piotr Krol <krolpiotr225@gmail.com>
2021-11-21 09:21:10 +0100

    Create interactive menu for calculator_parsing

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -4,6 +4,44 @@ namespace calculator
 {
     public class Calculator
     {
+        int numer1 = 0;
+        int numer2 = 0;
+        string menuCalculator = ("What would you like to do?\n" +
+            "0-Subtraction\n" +
+            "1-Addition\n" +
+            "2-Division\n" +
+            "2-Multiplication\n"
+            );
+
+
+
+
+
+
+                );
+
+
+        ChangeTheColorOfTheText.PrintColorMessage(ConsoleColor.DarkGreen, menuCalculator);
+        public static void SwithStatementforCalculator()
+        { 
+
+         switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Result: {Subtraction});
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1* num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+
+            }
+
         public int Subtraction(int number1, int number2)
         {
             return number1 - number2;
