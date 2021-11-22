bb63e44df0f505c62a0a51b219aae8309022f25d
PyLukasz <as.lukasz@gmail.com>
2021-11-22 09:21:28 +0100

    Adding a method to read a text file.

--- calculator/WorkingWithFiles.cs
+++ calculator/WorkingWithFiles.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace calculator
 {
     public class WorkingWithFiles
     {
+        public string ReadTextFile(string textFilePath)
+        {
+            return File.ReadAllText(textFilePath);
+        }
     }
 }
