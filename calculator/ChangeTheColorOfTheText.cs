43113581d81e1eb7038cd3c24d1e87a68e3e39e6
Piotr Krol <krolpiotr225@gmail.com>
2021-12-29 11:21:41 +0100

    All Console clear

--- calculator/ChangeTheColorOfTheText.cs
+++ calculator/ChangeTheColorOfTheText.cs
@@ -9,7 +9,8 @@ namespace calculator
     class ChangeTheColorOfTheText
     {
         public static void PrintColorMessage(ConsoleColor color, string message)
-        { 
+        {
+            Console.Clear();
             Console.ForegroundColor = color;
             Console.WriteLine(message);
             Console.ResetColor();
